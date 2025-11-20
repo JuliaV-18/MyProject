d83f35aeb39deac8e17b7db099357cedce63ec12
Student-2024 <student-2024@ashalim.org>
2025-11-20 11:50:56 +0200

    miri fixed some things

---
+++ app/src/main/java/com/lian/myproject/Splash.java
@@ -0,0 +1,38 @@
+package com.lian.myproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread mSplashThread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized (this) {
+                        wait(3000);
+                    }
+                }
+                catch(InterruptedException ex) {
+                }
+                finish();
+
+                Intent intent = new Intent(Splash.this, Login.class);
+                startActivity(intent);
+            }
+        };
+
+        mSplashThread.start();
+    }
+}
